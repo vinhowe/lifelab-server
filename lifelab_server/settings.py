38019efcf0a68e3f0a76e6aa023e8f1fcd6d1ddd
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-05-29 00:00:08 -0600

    Add crappy dev/prod variable

--- lifelab_server/settings.py
+++ lifelab_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from distutils.util import strtobool
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +75,18 @@ WSGI_APPLICATION = "lifelab_server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+database_name = (
+    "db-dev.sqlite3"
+    if "LL_DEV" in os.environ
+    else "db.sqlite3"
+)
+
+print(f"Using database {database_name}")
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, database_name),
     }
 }
 
